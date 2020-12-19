a36cd42305bfa23924a65a0fe8e554dcddb3e5c3
djsingh26 <dilraj3004@gmail.com>
2020-12-19 15:18:38 -0500

    first commit

---
+++ src/test/java/com/dj/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dj.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
